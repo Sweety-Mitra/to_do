3b531a51fb6818572396be3bbbf5cdf8723d720c
Sweety Mitra <118052909+Sweety-Mitra@users.noreply.github.com>
2026-04-16 14:04:15 +0530

    Undo and Redo implemented

--- src/App.js
+++ src/App.js
@@ -5,7 +5,15 @@ import TaskItem from './components/TaskItem';
 import { useState } from 'react';
 
 function App() {
-  const [tasks, setTasks] = useState([]);
+  // const [tasks, setTasks] = useState([]);
+
+  const [history, setHistory] = useState({
+    past: [],
+    present: [],
+    future: [],
+  });
+
+  const tasks = history.present;
 
   //Add Task Function
   const addTask = (task) => {
@@ -14,28 +22,30 @@ function App() {
       text: task,
       completed: false,
     };
-    setTasks((prev) => [...prev, newTask]);
+    const newTasks = [...tasks, newTask];
+    updateHistory(newTasks);
   }
 
   //Delete Task Function
   const deleteTask = (id) => {
-    setTasks((prev) => prev.filter(task => task.id !== id));
+    const newTasks = tasks.filter(task => task.id !== id);
+    updateHistory(newTasks);
   }
 
   //toggle Task Completion Function
   const toggleTask=(id) => {
-    setTasks((prev) => prev.map((task) =>
+    const newTasks = tasks.map((task) =>
       task.id === id ? {...task, completed: !task.completed} : task
-    ));
+    );
+    updateHistory(newTasks);
   };
 
   //Edit Task Function
   const editTask = (id, newText) => {
-    setTasks((prevTasks)=>
-      prevTasks.map((task)=>
-        task.id === id ? {...task, text: newText} : task
-      )
+    const newTasks = tasks.map((task) =>
+      task.id === id ? {...task, text: newText} : task
     );
+    updateHistory(newTasks);
   };
 
   //Derived State total no.od tasks
@@ -48,6 +58,46 @@ function App() {
   //no.of pending tasks
   const pendingTasks = tasks.filter(task => !task.completed).length;
 
+  //update history
+
+  const updateHistory = (newPresent) => {
+    setHistory((prev)=>({
+      past: [...prev.past, prev.present],
+      present: newPresent,
+      future: [],
+    }));
+  };
+
+  //Undo Function
+  const undo = () => {
+    setHistory((prev) => {
+      if (prev.past.length === 0) return prev;
+      const previous = prev.past[prev.past.length - 1];
+      const newPast = prev.past.slice(0,- 1);
+      return {
+        past: newPast,
+        present: previous,
+        future: [...prev.future, prev.present],
+      };
+    });
+  };
+
+
+  //Redo Function
+  const redo = () => {
+    setHistory((prev) => {
+      if (prev.future.length === 0) return prev;
+      const next = prev.future[0];
+      const newFuture = prev.future.slice(1);
+      return {
+        past: [...prev.past, prev.present],
+        present: next,
+        future: newFuture,
+      };
+    });
+  };
+
+
   return (
     <div>
       <h1>Smart task Manager</h1>
@@ -55,6 +105,10 @@ function App() {
         <p>Total Tasks: {totalTasks}</p>
         <p>Completed Tasks: {completedTasks}</p>
         <p>Pending Tasks: {pendingTasks}</p>
+      </div>
+      <div>
+        <button onClick={undo}>Undo</button>
+        <button onClick={redo}>Redo</button>
       </div>
       <TaskInput addTask={addTask} />
       <TaskList tasks={tasks} deleteTask={deleteTask} toggleTask={toggleTask} editTask={editTask} />
